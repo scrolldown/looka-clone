45fa033372c8653f3242c7c419d252f1a5ecb866
scrolldown <scrolldown@naver.com>
2023-07-14 00:18:50 +0900

    improve prompt to make words that is not included in keyword. and add newline in gpt response

--- src/page/Result.js
+++ src/page/Result.js
@@ -58,22 +58,34 @@ function Result() {
         setIsLoading(true)
         
         const scoreArray = [...new Set(Object.keys(result).map((k)=>result[k]))].sort(function(a, b){return b-a});
+        console.log(scoreArray)
         const highscoreKeywordArray = Object.keys(result).map(k => k).filter(keyword => scoreArray.slice(0,2).includes(result[keyword]))
     
         // ChatGPT API 요청 보내기
         const apiUrl = 'https://api.openai.com/v1/chat/completions';
-        const apiKey = 'api'; // ChatGPT API 키
+        const apiKey = process.env.REACT_APP_CHAT_GPT_API_KEY; // ChatGPT API 키 
         // 중요 ** APIKEY는 깃허브에 업로드 되면 안됨
-        const prompt = [{"role": "system", "content": "You are a competitive brand markerter."}
-                        ,{"role": "user", "content": "새로 오픈하는 가게의 브랜드 아이덴티티를 아래의 단어를 기반으로 3가지 이상 만들어줘 '"
+        const prompt = [
+                        //{"role": "system", "content": "You are a competitive brand markerter."},
+                        {"role": "user", "content": "당신은 우리 브랜드 디렉터 역할을 해주었으면 합니다. 나는 당신에게 브랜드 핵심가치와 그 핵심가치에 어울리는 브랜드 마케팅 문구를 요청할 것입니다. 브랜드 디렉터로서만 답을 해주었으면 좋겠습니다. 한 번에 모든 보존을 작성해주세요. 이번에 만드는 브랜드 아이덴티티를 '"
+                        +state.company.slogan+"' 슬로건과 키워드 '"
                         +highscoreKeywordArray.join(", ")
-                        +"'"}];
+                        +"'를 토대로 새로운 단어를 만들어주세요. 의미를 유지하되 더 창의적으로 만드십시오.\n"
+                        +" 답변을 해줄 때, 아래 조건을 지켜주세요. \n"
+                        +"1. 비슷한 단어 혹은 표현 반복 금지\n"
+                        +"2. 답변은 반드시 브랜드 핵심가치에 해당하는 표현이어야합니다. 그리고 그 포현이 왜 브랜드의 핵심가치인지 설명하는 문장을 함께 써주세요. \n"
+                        +"3. 답변의 형태는 [핵심가치, 단어,핵심가치에 대한 자세한 설명]로 출력해줘 \n"
+                        +"4. 핵심가치는 3개까지만 말해줘.\n"
+                        +"5. 답변을 모두 한국어로 적어줘.\n"
+                        +"6. 질문자의 브랜드에 도움이 될 만한 정보가 있다면, 웹으로 정보를 찾아서 링크를 함께 적어줘. : [URL(as '브랜키 팁')].\n"
+                        +"7. 마케팅에 쓸 한 문장을 핵심가치를 기반으로 만들어주세요.\n"
+                    }];
 
         try {
           const response = await axios.post(apiUrl, {
             "model": "gpt-3.5-turbo",
             "messages": prompt,
-            "temperature": 0.7
+            "temperature": 0.1
             }, {
             headers: {
               'Content-Type': 'application/json',
@@ -85,7 +97,7 @@ function Result() {
           const chatResponse = response.data.choices[0].message.content;
     
           // ChatGPT API 응답 처리
-          setChatHistory([...chatHistory, { user: prompt[1]['content'], bot: chatResponse }]);
+          setChatHistory([...chatHistory, { user: prompt[0]['content'], bot: chatResponse }]);
           setIsLoading(false);
         } catch (error) {
           console.error('ChatGPT API 요청 실패:', error);
@@ -272,16 +284,19 @@ function Result() {
             : < div className="chat-history">
                     {chatHistory.map((chat, index) => (
                     <div key={index}>
-                        <p>user: {chat.user}</p>
-                        <p>gpt: {chat.bot}</p>
+                        <p>user: {chat.user.split("\n").map((line)=>{
+                            return(<span>{line}<br/></span>)
+                        })}</p>
+                        <p>gpt: {chat.bot.split("\n").map((line)=>{
+                            return(<span>{line}<br/></span>)
+                        })}</p>
                     </div>
                     ))}
                 </div>
             }
 
 
-
-
+            
             <Button className='btn btn-default' onClick={()=>window.print()}>print</Button> 
 
 
